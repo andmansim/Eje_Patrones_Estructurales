eef02046472cddfe3e22ff5e07b4df92f650bb06
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-15 16:02:23 +0100

    cambios

--- Pizzeria/app/routes.py
+++ Pizzeria/app/routes.py
@@ -8,5 +8,8 @@ from codigoPizza import funciones_main
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+def pizzapersonalizada():
     pizza = director.build_pizza() #Le decimos al chef los pasos a seguir para dicha pizza
-    return render_template('index.html', pizza = pizza)
+    return render_template('Pizzassueltas.html', pizza = pizza)
