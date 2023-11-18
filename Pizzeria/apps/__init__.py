a0044bff85c369d4f4f96b1a95a4f16da547dfb6
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-18 12:55:08 +0100

    jnjjn

--- Pizzeria/apps/__init__.py
+++ Pizzeria/apps/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from apps import routes
-from codigoPizza import builders
 
 app = Flask(__name__)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+from apps import routes
