ccb708fafc11b4fb2311dc868875a79d8266e86f
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-19 19:34:49 +0100

    no me convence

--- Pizzeria/apps/__init__.py
+++ Pizzeria/apps/__init__.py
@@ -2,12 +2,13 @@
 Este archivo se encarga de conectar la web con el código
 '''
 from flask import render_template, request, redirect, Flask
-from codigoPizza import builders
+from codigoPizza import builders, manejardatos
 
 app = Flask(__name__)
 
 director = builders.Director() #Chef
 builder = builders.ConcreteBuilder1() #Tipo de pizza
+web_pizza = manejardatos.WebPizzeria()
 
 @app.route('/home')
 def home():
@@ -20,6 +21,7 @@ def pizzapersonalizada():
 @app.route('/form   ', methods=['POST']) #recibimos los datos en el fichero procesar_pizza
 def procesar_pizza():
     print(request.get_data()) #imprimimos los datos recibidos
+    #recogemos los datos en las dinstintas variables
     masa = request.form.get('masa')
     salsa = request.form.get('salsa')
     ingredientes = request.form.get('ingredientes')
@@ -29,13 +31,11 @@ def procesar_pizza():
     postre = request.form.get('postre')
 
     
-    #le pasamos los datos a la clase builder
-
+    #Contruimos la pizza
     director.builder = builder #Le decimos al chef que tipo de pizza queremos
     director.build_pizza(masa, salsa, ingredientes, coccion, presentacion, bebida, postre) #Le decimos al chef los pasos a seguir para dicha pizza
     builder.pizza.list_parts()
     a = builder.pizza.get_parts() #Lista con todos los datos de la pizza
-    print(a)
 
 if __name__ == '__main__':
     app.run(debug=True)
