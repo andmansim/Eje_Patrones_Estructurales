4b120bb0f2b05fb4bf76d452662686a66efb19e3
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-20 12:37:33 +0100

    cambio y añado id

--- Pizzeria/apps/codigoPizza/pedido_pizza.py
+++ Pizzeria/apps/codigoPizza/pedido_pizza.py
@@ -1,17 +1,21 @@
 import os
 import csv
+
 def guardar_pedido_en_csv(id_cliente, datos_pedido):
     # Define la ruta del archivo CSV basada en el ID del cliente
-    archivo_csv = f"pedidos_cliente_{id_cliente}.csv"
+    archivo_csv = f"pedidos_clientes.csv"
 
     # Comprueba si el archivo ya existe
-    if not os.path.exists(archivo_csv):
-        # Si no existe, crea un nuevo archivo CSV y escribe el encabezado
-        with open(archivo_csv, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(["Masa", "Salsa", "Ingredientes", "Cocción", "Presentación", "Bebida", "Postre"])
+    nuevo_archivo = not os.path.exists(archivo_csv)
 
     # Agrega el nuevo pedido al archivo CSV
     with open(archivo_csv, mode='a', newline='') as file:
         writer = csv.writer(file)
-        writer.writerow(datos_pedido)
+
+        # Si es un nuevo archivo, escribe el encabezado
+        if nuevo_archivo:
+            writer.writerow(["id_cliente", "Masa", "Salsa", "Ingredientes", "Cocción", "Presentación", "Bebida", "Postre"])
+
+        # Agrega el nuevo pedido con el id_cliente
+        writer.writerow([id_cliente] + datos_pedido)
+
