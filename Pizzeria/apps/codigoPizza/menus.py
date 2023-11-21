217de974c0184dafe465f5999e01c5c4c572a707
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-21 11:22:12 +0100

    q asco

--- Pizzeria/apps/codigoPizza/menus.py
+++ Pizzeria/apps/codigoPizza/menus.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Any #tipado dinámigo 
-
+from composite import Component, Leaf, Composite
 from abc import ABC, abstractmethod
 
 class Producto(ABC):
@@ -9,7 +9,7 @@ class Producto(ABC):
     Encargado de definir la estructura común de los productos
     '''
     @abstractmethod
-    def obtener_precio(self):
+    def construir(self):
         pass
 
 class ElementoMenu(Producto):
@@ -17,21 +17,22 @@ class ElementoMenu(Producto):
     Son los elementos inidviduales que puede elegir el cliente, es decir, 
     la bebida o postre que quiera personalizar al pedir su combo personalizado
     '''
-    def __init__(self, nombre, precio):
+    def __init__(self, nombre):
         self.nombre = nombre
-        self.precio = precio
 
-    def obtener_precio(self):
-        return self.precio
+    def construir(self):
+        return Composite(self.nombre)
 
 class Pizza(ElementoMenu):
     '''
     Subclase de ElementoMenu, donde se define la pizza, aunque 
     no se si ponerla como las distintas que hay o crearla sin más
     '''
-    def __init__(self, nombre, ingredientes, precio):
-        super().__init__(nombre, precio)
+    def __init__(self, nombre, ingredientes):
+        super().__init__(nombre)
         self.ingredientes = ingredientes
+    def construir(self):
+        return Composite(self.nombre)
 
 class MenuCompuesto(Producto):
     '''
@@ -45,9 +46,11 @@ class MenuCompuesto(Producto):
     def agregar_elemento(self, elemento):
         self.elementos.append(elemento)
 
-    def obtener_precio(self):
-        return sum(elemento.obtener_precio() for elemento in self.elementos)
-
+    def construir(self):
+        menu = Composite(self.codigo, self.nombre)
+        for elemento in self.elementos:
+            menu.agregar_elemento(elemento.construir())
+        return menu
 class Builder(ABC): 
     '''
     Clase abstracta builder que se encarga de construir y obtener el menú
@@ -68,9 +71,9 @@ class MenuGenericoBuilder(Builder):
         self.menu = MenuCompuesto("MG", "Menú Genérico")
 
     def construir_menu(self):
-        pizza_margarita = Pizza("Margarita", ["tomate", "queso", "albahaca"], 12.0)
-        bebida = ElementoMenu("Bebida Genérica", 3.0)
-        postre = ElementoMenu("Postre Genérico", 4.0)
+        pizza_margarita = Pizza("Margarita", ["tomate", "queso", "albahaca"])
+        bebida = ElementoMenu("Bebida Genérica")
+        postre = ElementoMenu("Postre Genérico")
 
         self.menu.agregar_elemento(pizza_margarita)
         self.menu.agregar_elemento(bebida)
@@ -108,28 +111,3 @@ class RestauranteDirector:
     def obtener_menu(self):
         return self.builder.obtener_menu()
 
-# Ejemplo de uso:
-restaurante = RestauranteDirector(MenuGeneridoBuilder())
-restaurante.construir_menu()
-menu_generico = restaurante.obtener_menu()
-
-restaurante = RestauranteDirector(MenuPersonalizableBuilder())
-pizza_personalizable = Pizza("Personalizada", ["tomate", "queso"], 15.0)
-bebida_personalizable = ElementoMenu("Bebida Personalizable", 4.0)
-postre_personalizable = ElementoMenu("Postre Personalizable", 5.0)
-restaurante.builder.construir_menu(pizza_personalizable, bebida_personalizable, postre_personalizable)
-menu_personalizable = restaurante.obtener_menu()
-
-# Agregar menús al menú principal
-restaurante.menu_principal = MenuCompuesto("MP", "Menú Principal")
-restaurante.menu_principal.agregar_elemento(menu_generico)
-restaurante.menu_principal.agregar_elemento(menu_personalizable)
-
-# Calcular el precio de un menú por su código
-codigo_menu_buscar = "MP"
-precio_menu = restaurante.menu_principal.obtener_precio()
-
-if precio_menu is not None:
-    print(f"El precio del menú {codigo_menu_buscar} es: ${precio_menu}")
-else:
-    print(f"No se encontró el menú con el código {codigo_menu_buscar}")
