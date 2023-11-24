c11a517a9cc4730fd816b24579b08816fe36ce24
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-24 11:19:54 +0100

    funca

--- Pizzeria/apps/codigoPizza/guard_pedido.py
+++ Pizzeria/apps/codigoPizza/guard_pedido.py
@@ -30,21 +30,3 @@ def guardar_pedido(id_cliente, datos_pedido, dato):
         writer.writerow([id_cliente] + datos_pedido)
 
 
-def guardar_pedido_combo(id_cliente, datos_pedido):
-    # Define la ruta del archivo CSV basada en el ID del cliente
-    archivo_csv = "pedidos_combo.csv"
-
-    # Comprueba si el archivo ya existe
-    nuevo_archivo = not os.path.exists(archivo_csv)
-
-    # Agrega el nuevo pedido al archivo CSV
-    with open(archivo_csv, mode='a', newline='') as file:
-        writer = csv.writer(file)
-
-        # Si es un nuevo archivo, escribe el encabezado
-        if nuevo_archivo:
-            writer.writerow(["id_cliente", 'nombre', 'bebida', 'postre', 'pizza', 'precio'])
-
-        # Agrega el nuevo pedido con el id_cliente
-        writer.writerow([id_cliente] + datos_pedido)
-
