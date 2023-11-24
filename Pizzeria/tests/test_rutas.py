5f93d3b9637a20e95685eb3132c130882a7ebd46
andmansim <91722324+andmansim@users.noreply.github.com>
2023-11-24 13:10:25 +0100

    no va

--- Pizzeria/tests/test_rutas.py
+++ Pizzeria/tests/test_rutas.py
@@ -1,14 +1,12 @@
 import unittest
 from unittest.mock import patch
-from rutas import app
+from apps.rutas import app, datos_pizza_per
 
 class TestDatosPizzaPer(unittest.TestCase):
-    @patch('app.secrets.token_hex', return_value = 'mocked_token')
-    @patch('app.request')
+    @patch('apps.rutas.secrets.token_hex', return_value='mocked_token')
+    @patch('apps.rutas.guardar_pedido')
     def test_datos_pizza_per(self, mocked_guardar_pedido, mocked_token_hex):
-        # Configura la solicitud simulada
         with app.test_client() as client:
-            # Simula una solicitud POST con datos específicos
             response = client.post('/datos_pizza_per', data={
                 'masa': 'fina',
                 'salsa': 'tomate',
@@ -20,11 +18,8 @@ class TestDatosPizzaPer(unittest.TestCase):
                 'postre': 'flan',
             })
 
-        # Realiza afirmaciones (assertions) para verificar el comportamiento esperado
         self.assertIn('¡Datos del pedido procesados con éxito!', response.data)
         mocked_guardar_pedido.assert_called_once_with('mocked_token', ..., 'pizza')  # Ajusta los argumentos según tu lógica
-
-    # Agrega más métodos de prueba según sea necesario para otras combinaciones de datos
 
 if __name__ == '__main__':
     unittest.main()
